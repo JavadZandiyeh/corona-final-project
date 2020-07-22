56303b1d2eec20929ff2c14ddf6f9b8bdae7a303
Mohammad javad zandiyeh <58389811+JavadZandiyeh@users.noreply.github.com>
2020-07-22 15:34:27 +0430

    make last works functional,and some works on file

--- hello.c
+++ hello.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <libpq-fe.h>
+#include<dirent.h>
+#include <string.h>
+
+
+//public variables
+PGresult *res;
+PGconn *conn;
+
+//starting connection with data base
+void connectingToDB(){
+    //The PGconn encapsulates the connection to database
+    //mean we started a connection to databas with bellow details
+    //here we connect to default data base of postgresql (mean postgres) and after
+    //that we will create a new data base and switch on it
+    conn = PQconnectdb("host=localhost dbname=postgres user=postgres password=javad-9831032");	
+
+    //The PQstatus() function checks the status of connection to database
+    //and if is BAD it shows the error and finishes the connection
+    //then program finishes
+    if (PQstatus(conn) == CONNECTION_BAD) {
+
+        fprintf(stderr, "Connection to database failed: %s\n",
+            PQerrorMessage(conn));
+
+        PQfinish(conn);
+        exit(1);
+    }
+}
+
 
 //this function is for finishing connection with data base
-void do_exit(PGconn *conn, PGresult *res, PGresult *res_1, PGresult *res_2) {
+void do_exit() {
     
     fprintf(stderr, "%s\n", PQerrorMessage(conn));    
 
     //clearing resualt of queries
     PQclear(res);
-    PQclear(res_1);
-    PQclear(res_2);
-
+    
     //finishing connection
     PQfinish(conn);    
     
@@ -19,53 +46,89 @@ void do_exit(PGconn *conn, PGresult *res, PGresult *res_1, PGresult *res_2) {
     exit(1);
 }
 
-//main function of program
-int main() {
- 
-    //The PGconn encapsulates the connection to database
-    //mean we started a connection to databas with bellow details
-    PGconn *conn = PQconnectdb("host=localhost dbname=postgres user=postgres password=javad-9831032"); 
 
-    //The PQstatus() function checks the status of connection to database
-    //and if is BAD it shows the error and finishes the connection
-    //then program finishes
-    if (PQstatus(conn) == CONNECTION_BAD) {
-        
-        fprintf(stderr, "Connection to database failed: %s\n",
-            PQerrorMessage(conn));
-            
-        PQfinish(conn);
-        exit(1);
-    }
-
-    //The PGresult encapsulates the result of a query
-    //The PQexec() function submits an SQL command to the server and waits for the result
-    //here we want to drop or remove last tables which have names that we want to make our new tables
-    PGresult *res = PQexec(conn, "DROP TABLE IF EXISTS fp_stores_data");
-    PGresult *res_1 = PQexec(conn, "DROP TABLE IF EXISTS fp_city_aggregation");
-    PGresult *res_2 = PQexec(conn, "DROP TABLE IF EXISTS fp_store_aggregation");
-
-
+void checkResualtStatus(){
     //The PQresultStatus() function should be called to check the return value for any errors.
     //The PGRES_COMMAND_OK is returned if the command was properly executed and it does not return data.
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);
+        do_exit();
     }
 
-    //clearing result of queries     
+    //clearing result of queries
     PQclear(res);
-    PQclear(res_1);
-    PQclear(res_2);
+}
 
 
-    //creating tables............................................I am here..............................................
-    res = PQexec(conn, "CREATE TABLE fp_stores_data(time INTEGER,province Varchar(50),city,market_id,product_id,price,quantity, has_sold)");
-       
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res); 
-    }
+//create new data base and switch on it
+void createDB(){
+    res = PQexec(conn, "CREATE DATABASE fpdb ENCODING 'UTF8'"); 
+    checkResualtStatus();
+
+
+    //change data base to 'fpdb'
+    res = PQexec(conn, "\c fpdb");
+    checkResualtStatus();
+}
+
+
+//creating tables we need
+void createTables(){
+    res = PQexec(conn, "CREATE TABLE fp_stores_data(time bigint, province varchar(50), city varchar(50), market_id integer,product_id integer,price integer, quantity integer, has_sold integer)");
+    checkResualtStatus();
     
-    PQclear(res);
+    //............................................problem here.......................
+
+
+}
+
+
+void readFiles(){
+
+    char dirName[100];
+    strcpy(dirName, "/tmp/final_project/");
+
+    //dirent.h header file contains variables and functions related to directory streams.
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(dirName);
+    
+    if (d){
+        while ((dir = readdir(d)) != NULL){
+	    
+	    //finding the name of file
+	    char file[100];
+	    strcpy(file, "tmp\\final_project\\");
+	    //dir->d_name is the name of a file in (d) directory 
+	    strcat(file, dir->d_name);
+
+	    //pointer to file
+	    FILE *fp = fopen (file, "r");
+	    
+	    //reading all lines of file
+	    char line[256];
+     	    while (fgets(line, sizeof(line), fp)) {
+		    //printf("%s", line);
+	    	//..................i have to insert into tables.................
+	    
+	    }
+
+	    fclose(fp);
+	    remove(file)
+    	}
+        closedir(d);
+    }
+}
+
+
+//main function of program
+int main() {
+ 
+    connectingToDB();
+    createDB();
+    createTables();
+    readFiles();
+
+    //...........................................
     
     res = PQexec(conn, "INSERT INTO Cars VALUES(1,'Audi',52642)");
         
