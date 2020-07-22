d737e06f612615047b8d041beac85fa069cb95bc
Mohammad javad zandiyeh <58389811+JavadZandiyeh@users.noreply.github.com>
2020-07-22 18:30:01 +0430

    create tables in created data base
    till now :
    1)connect to default data base
    2)create a new data base
    3)switch to new data base
    4)create tables in new data base

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 #include <libpq-fe.h>
 #include<dirent.h>
 #include <string.h>
-
+#include <unistd.h>
 
 //public variables
 PGresult *res;
@@ -50,9 +50,8 @@ void do_exit() {
 void checkResualtStatus(){
     //The PQresultStatus() function should be called to check the return value for any errors.
     //The PGRES_COMMAND_OK is returned if the command was properly executed and it does not return data.
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
         do_exit();
-    }
 
     //clearing result of queries
     PQclear(res);
@@ -61,137 +60,161 @@ void checkResualtStatus(){
 
 //create new data base and switch on it
 void createDB(){
-    res = PQexec(conn, "CREATE DATABASE fpdb ENCODING 'UTF8'"); 
+
+    res = PQexec(conn, "DROP DATABASE IF EXISTS fpdb");
     checkResualtStatus();
 
 
-    //change data base to 'fpdb'
-    res = PQexec(conn, "\c fpdb");
+    res = PQexec(conn, "CREATE  DATABASE fpdb ENCODING 'UTF8'"); 
     checkResualtStatus();
+
+    PQfinish(conn);	
+
+
+    conn = PQconnectdb("host=localhost dbname=fpdb user=postgres password=javad-9831032");
+
+    if (PQstatus(conn) == CONNECTION_BAD) {
+
+        fprintf(stderr, "Connection to database failed: %s\n",
+            PQerrorMessage(conn));
+
+        PQfinish(conn);
+        exit(1);
+    }
 }
 
 
 //creating tables we need
 void createTables(){
-    res = PQexec(conn, "CREATE TABLE fp_stores_data(time bigint, province varchar(50), city varchar(50), market_id integer,product_id integer,price integer, quantity integer, has_sold integer)");
+
+    //deleting last tables with simiular name as tables want to create 
+    res = PQexec(conn, "DROP TABLE IF EXISTS fp_stores_data");
     checkResualtStatus();
+
+    res = PQexec(conn, "DROP TABLE IF EXISTS fp_city_aggregation");
+    checkResualtStatus();
+
+    res = PQexec(conn, "DROP TABLE IF EXISTS fp_store_aggregation");
+    checkResualtStatus();
+
+
     
-    //............................................problem here.......................
+    res = PQexec(conn, "CREATE TABLE fp_stores_data(time integer, province varchar(50), city varchar(50), market_id integer,product_id integer,price integer, quantity integer, has_sold integer, PRIMARY KEY(time, province, city, market_id, product_id, price, quantity, has_sold))");
+    checkResualtStatus();
 
+    res = PQexec(conn, "CREATE TABLE fp_city_aggregation(time integer, city varchar(50), price integer, quantity integer, has_sold integer)");
+    checkResualtStatus();
 
+    res = PQexec(conn, "CREATE TABLE fp_store_aggregation (market_id integer, price integer, has_sold integer)");
+    checkResualtStatus();
 }
 
+void insertIntoTables(int time, char province[50], char city[50], int market_id, int product_id, int price, int quantity, int has_sold){  
+
+    char command[256];
+    sprintf(command, "INSERT INTO fp_stores_data VALUES(%d, '%s', '%s', %d, %d, %d, %d, %d)", time, province, city, market_id, product_id,  price, quantity, has_sold);
+    res = PQexec(conn, command);
+    checkResualtStatus();
+
+
+    char command_1[256];
+    sprintf(command_1, "INSERT INTO fp_city_aggregation VALUES(%d, '%s', %d, %d, %d)", time, city, price, quantity, has_sold);
+    res = PQexec(conn, command_1);
+    checkResualtStatus();
+
+
+    char command_2[256];
+    sprintf(command_2, "INSERT INTO fp_store_aggregation VALUES(%d, %d, %d)", market_id, price, has_sold);
+    checkResualtStatus();
+}
 
 void readFiles(){
-
-    char dirName[100];
-    strcpy(dirName, "/tmp/final_project/");
 
     //dirent.h header file contains variables and functions related to directory streams.
     DIR *d;
     struct dirent *dir;
-    d = opendir(dirName);
+    d = opendir("/tmp/final_project/");
     
     if (d){
-        while ((dir = readdir(d)) != NULL){
+//         while(1){
+      
+	      while ((dir = readdir(d)) != NULL){
 	    
-	    //finding the name of file
-	    char file[100];
-	    strcpy(file, "tmp\\final_project\\");
-	    //dir->d_name is the name of a file in (d) directory 
-	    strcat(file, dir->d_name);
+      	          //finding name of file
+		  //dir->d_name is the name of a file in (d) directory
+	          char file[100] = "tmp\\final_project\\"; 
+	          strcat(file, dir->d_name);
 
-	    //pointer to file
-	    FILE *fp = fopen (file, "r");
-	    
-	    //reading all lines of file
-	    char line[256];
-     	    while (fgets(line, sizeof(line), fp)) {
-		    //printf("%s", line);
-	    	//..................i have to insert into tables.................
-	    
-	    }
 
-	    fclose(fp);
-	    remove(file)
-    	}
+	          //pointer to file
+	          FILE *fp = fopen (file, "r");
+	    
+	          //reading file line by line
+	          char line[256];
+     	          while (fgets(line, sizeof(line), fp)) {
+		      
+		      int i;		      
+		      for(i = 0; i < strlen(line); i++){
+		          int time;
+			  char province[50];
+			  char city[50];
+			  int market_id;
+			  int product_id;
+			  int price;
+			  int quantity;
+			  int has_sold;
+			
+			  fscanf(fp, "%d", &time);
+			  fgetc(fp);
+
+			  fgets(province, 50, fp);
+			  fgetc(fp);
+
+			  fgets(city, 50, fp);
+			  fgetc(fp);
+
+			  fscanf(fp, "%d", &market_id);
+                          fgetc(fp);
+
+			  fscanf(fp, "%d", &product_id);
+                          fgetc(fp);
+
+			  fscanf(fp, "%d", &price);
+                          fgetc(fp);
+
+			  fscanf(fp, "%d", &quantity);
+                          fgetc(fp);
+
+			  fscanf(fp, "%d", &has_sold);
+		 	
+			  insertIntoTables(time, province, city, market_id, product_id, price, quantity, has_sold);
+
+
+		      }//end of --> for(i = 0; i < strlen(line); i++)
+		  	    
+	         }//end of --> while (fgets(line, sizeof(line), fp))
+
+	         fclose(fp);
+	         remove(file);
+    	    }//end of --> while ((dir = readdir(d)) != NULL)
+//	    sleep(65);
+//        }//end of --> while(1)
         closedir(d);
-    }
-}
+    }//end of --> if (d)
+}//end of function
 
 
 //main function of program
 int main() {
  
     connectingToDB();
+    
     createDB();
+    
     createTables();
-    readFiles();
-
-    //...........................................
     
-    res = PQexec(conn, "INSERT INTO Cars VALUES(1,'Audi',52642)");
-        
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) 
-        do_exit(conn, res);     
-    
-    PQclear(res);    
-    
-    res = PQexec(conn, "INSERT INTO Cars VALUES(2,'Mercedes',57127)");
-        
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
-    }
-    
-    PQclear(res);    
-    
-    res = PQexec(conn, "INSERT INTO Cars VALUES(3,'Skoda',9000)");
-        
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
-    }
-    
-    PQclear(res);  
-    
-    res = PQexec(conn, "INSERT INTO Cars VALUES(4,'Volvo',29000)");
-        
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
-    }
-    
-    PQclear(res);      
-    
-    res = PQexec(conn, "INSERT INTO Cars VALUES(5,'Bentley',350000)");
-        
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
-    }
-    
-    PQclear(res);  
-    
-    res = PQexec(conn, "INSERT INTO Cars VALUES(6,'Citroen',21000)");
-        
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
-    }
-    
-    PQclear(res);  
-    
-    res = PQexec(conn, "INSERT INTO Cars VALUES(7,'Hummer',41400)");
-        
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
-    }
-    
-    PQclear(res);  
-    
-    res = PQexec(conn, "INSERT INTO Cars VALUES(8,'Volkswagen',21600)");
-        
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
-    }
-    
-    PQclear(res);  
+    //readFiles();
+   
     PQfinish(conn);
 
     return 0;
